ff36db956eea448f9b41812160f42c767742b560
biu <hezhou920@gmail.com>
2017-11-04 21:43:11 +0800

    完善后台处理

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const http = require('http');
 const multiparty = require('multiparty');
 const path = require('path');
 const fs = require('fs');
-const sharp = require('sharp');
+const Sharp = require('sharp');
 const uuidv1 = require('uuid/v1');
+const mkdirp = require('mkdirp');
 
 const app = express();
 
@@ -21,26 +22,67 @@ server.listen(port, function () {
   console.log(`Server passport listening on port: ${port}`);
 });
 
+const UPLOAD_DIR = './public/uploads/';
+
 function handleUpload(req, res, next) {
-  const uploadDir = './public/uploads/';
   const form = new multiparty.Form({
-    uploadDir: uploadDir,
     maxFilesSize: 2 * 1024 * 1024
   });
 
   form.parse(req, function (err, fields, files) {
     if (err) {
-      console.log(err);
-      return res.json('文件大小超过 2 MB');
-    }
-    const file = files.avatar[0];
-    sharp(file.path).resize(638).toFile(uploadDir + uuidv1() + '.jpeg', function (err) {
-      if (err) {
-        console.log(err);
-        return res.json('上传失败');
+      console.error(err);
+      if (err.code === 'ETOOBIG') {
+        res.json({
+          state: 'fail',
+          message: '文件大小不能超过 2 M'
+        })
+      } else {
+        res.json({
+          state: 'fail',
+          message: '服务器错误'
+        });
       }
-      fs.unlink(file.path);
-      res.json('上传成功');
+      return;
+    }
+
+    const avatar = files.avatar && files.avatar[0];
+    const fileType = avatar.headers['content-type'];
+    if (fileType.split('/')[0] !== 'image') {
+      fs.unlink(avatar.path, function (err) {
+        if (err) {
+          console.error(err);
+        }
+      });
+      return res.json({
+        state: 'fail',
+        message: '文件格式错误'
+      });
+    }
+    console.log('成功接收图片 %s，开始 Resize', avatar.originalFilename);
+
+    mkdirp.sync(UPLOAD_DIR);
+    const newName = uuidv1() + '.jpeg';
+    const newPath = path.join(UPLOAD_DIR, newName);
+    Sharp(avatar.path).resize(600).toFile(newPath, function (err, info) {
+      if (err) {
+        console.error(err);
+        res.json({
+          state: 'fail',
+          message: '服务器错误'
+        });
+      } else {
+        console.log('成功 Resize 图片！图片信息为：' + JSON.stringify(info));
+        res.json({
+          state: 'success',
+          message: '图片上传成功',
+          data: {
+            width: info.width,
+            height: info.height,
+            path: '/uploads/avatars/' + newName
+          }
+        });
+      }
     });
   });
 }
