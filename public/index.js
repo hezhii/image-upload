0df997bb1c2b22fe5e2c4dcf9019d09e185f36a1
biu <hezhou920@gmail.com>
2017-12-01 22:31:35 +0800

    进度条

--- public/index.js
+++ public/index.js
@@ -20,13 +20,22 @@
       if (xhr.status === 200) {
         const res = xhr.response;
         if (res.state === 'success') {
-          alert('上传成功！图片地址为: ' + res.data.path);
+          console.log('上传成功！图片地址为: ' + res.data.path);
         } else {
           alert('上传失败！' + res.message);
         }
       } else {
         alert(xhr.status + ': ' + xhr.statusText);
       }
+    };
+    xhr.upload.onloadstart = function () {//上传开始执行方法
+      document.getElementById('process').style.display = 'block';
+    };
+    xhr.upload.onprogress = function (e) {
+      let total = e.total;
+      let loaded = e.loaded;
+      let percentage = Math.floor(loaded / total * 100) + '%';
+      document.getElementById('processBg').style.width = percentage;
     };
     xhr.send(formData);
   };
